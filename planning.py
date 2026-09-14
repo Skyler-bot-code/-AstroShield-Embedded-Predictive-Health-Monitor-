a143af68b232250a2f9f823806676ba01b7f2eac
Skyler <chiangskyler@gmail.com>
2026-09-13 18:26:41 -0700

    9/13

---
+++ planning.py
@@ -0,0 +1,14 @@
+
+
+# 1. Gather hardware components
+# 2. Set up the hardware connection
+# 3. Read + Organize raw sensor data (1)
+# 4. Dynamic time hypothesis testing for anamoly detection <-Revise current built 
+# 5. Linear regression analysis (bivariate and multivariate) for trend analysis <- Revise current built
+# 6. Visualization for Mission Control dashboard (Outputting lines of values + Charts and graphs)
+# 7. Debugging and testing failures
+
+
+
+
+
